f04efd3067f59a368b6132d65db3b774eb75ce28
Tumi <oluwatumininuojo25@gmail.com>
2021-02-16 05:44:07 +0100

    update

--- app/src/main/java/com/tumininu/verito/ui/MainActivity.kt
+++ app/src/main/java/com/tumininu/verito/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.tumininu.verito.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.tumininu.verito.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/tumininu/verito/ui/tasks/TaskAdapter.kt
@@ -0,0 +1,46 @@
+package com.tumininu.verito.ui.tasks
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.tumininu.verito.data.Task
+import com.tumininu.verito.databinding.ItemTaskBinding
+
+class TaskAdapter :
+    androidx.recyclerview.widget.ListAdapter<Task, TaskAdapter.TaskViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val binding = ItemTaskBinding.inflate(
+            LayoutInflater.from(parent.context), parent,
+            false
+        )
+        return TaskViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class TaskViewHolder(private val binding: ItemTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(task: Task) {
+            binding.apply {
+                checkBoxCompleted.isChecked = task.completed
+                textViewName.text = task.name
+                textViewName.paint.isStrikeThruText = task.completed
+                labelPriority.isVisible = task.important
+            }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean = oldItem == newItem
+    }
+}

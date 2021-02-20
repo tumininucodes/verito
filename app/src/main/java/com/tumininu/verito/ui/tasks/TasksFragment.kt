4f928b0e2d4a47a61b935286b03455bc71d0b166
Tumi <oluwatumininuojo25@gmail.com>
2021-02-20 14:59:15 +0100

    update

--- app/src/main/java/com/tumininu/verito/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/tumininu/verito/ui/tasks/TasksFragment.kt
@@ -1,12 +1,37 @@
 package com.tumininu.verito.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tumininu.verito.R
+import com.tumininu.verito.databinding.FragmentTaskBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_task) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTaskBinding.bind(view)
+
+        val taskAdapter = TaskAdapter()
+
+        binding.apply {
+            recyclerViewTask.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/com/tumininu/verito/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/tumininu/verito/ui/tasks/TasksViewModel.kt
@@ -2,5 +2,12 @@ package com.tumininu.verito.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.tumininu.verito.data.TasksDao
 
-class TasksViewModel @ViewModelInject constructor() : ViewModel()
+class TasksViewModel @ViewModelInject constructor(
+    private val tasksDao: TasksDao
+) : ViewModel() {
+
+    val tasks = tasksDao.getTasks().asLiveData()
+}
